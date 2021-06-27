55e4c7e3ff920f2bad4b053522e9c1faa593e0f8
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-27 16:35:55 -0400

    user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resource :users, only: [:create]
+  # login route
+  post "/login", to: "users#login"
+  # test login route
+  get "/auto_login", to: "users#auto_login"
 end
+
+# post "/url_point", to: controller#function
