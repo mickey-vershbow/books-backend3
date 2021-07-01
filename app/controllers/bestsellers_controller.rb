d622416d578345f1deaa5bf6ecee06d5e899722c
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-06-30 20:56:16 -0400

    bestseller route still requiring auth

--- app/controllers/bestsellers_controller.rb
+++ app/controllers/bestsellers_controller.rb
@@ -1,5 +1,5 @@
 class BestsellersController < ApplicationController
-
+    
 def current_list
     @response = Faraday.get "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=7FxA5tYHL2GPOCHfddNn3uVDouwtx71d"
 
