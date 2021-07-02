448be30987db35c0303c5c41fe6b0ac14af75b8c
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-07-02 09:43:21 -0400

    cleaned up bestsellers_controller map attempt

--- app/controllers/bestsellers_controller.rb
+++ app/controllers/bestsellers_controller.rb
@@ -7,21 +7,17 @@ class BestsellersController < ApplicationController
         response = Faraday.get(url)
         @response_result = JSON.parse(response.body)
 
+        @books_array = []
 
+        @response_result.results.books.map do |book|
+            @container = {}
 
-    #     @books_array = []
+        @container.rank = book.rank
+        @container.rank_last_week = book.rank_last_week
 
-    #     @response_result.results.books.map do |book|
-    #         @container = {}
-
-
-    # # #     # books.results.books[]
-    #     @container.rank = book.rank
-    #     @container.rank_last_week = book.rank_last_week
-   
-    #     @books_array.push(container)
-    #     p @books_array
-    #     end
+        @books_array.push(container)
+        p @books_array
+        end
         render json: @response_result
     end
 end
