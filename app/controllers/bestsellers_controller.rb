addf483fca916c28175f0639932c87ed8ed0252f
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-07-06 16:46:28 -0400

    orking previous_list function and api call. Need to figure out how to grab user input for date isntead of hard-coding it in the function

--- app/controllers/bestsellers_controller.rb
+++ app/controllers/bestsellers_controller.rb
@@ -2,32 +2,10 @@
 class BestsellersController < ApplicationController
 
     require 'faraday'
-
-    # def current_list
-    #     current_url = ENV["NYT_API_KEY_CURRENT"]
-    #     response = Faraday.get(current_url)
-    #     @response_result = JSON.parse(response.body, { object_class: OpenStruct })
-
-    #     @final_result = @response_result.results.books.map do |book|
-
-    #         container = {
-    #             rank: book[:rank],
-    #             rank_last_week: book[:rank_last_week],
-    #             weeks_on_list: book[:weeks_on_list],
-    #             publisher: book[:publisher],
-    #             description: book[:description],
-    #             title: book[:title],
-    #             author: book[:author],
-    #             book_image: book[:book_image],
-    #             # TODO: add buy_links
-    #         }
-
+    API_KEY = ENV["NYT_API_KEY_CURRENT"]
 
         def current_list
-            api_key = ENV["NYT_API_KEY_CURRENT"]
-            p api_key
-            current_url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{api_key}"
-            p current_url
+            current_url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{API_KEY}"
             response = Faraday.get(current_url)
             @response_result = JSON.parse(response.body, { object_class: OpenStruct })
 
@@ -44,37 +22,31 @@ class BestsellersController < ApplicationController
                     book_image: book[:book_image],
                     # TODO: add buy_links
                 }
-
             end
             render json: @final_result
     end
 
-    #     end
-    #     render json: @final_result
-    # end
+            def previous_list
+            date = "2017-05-27"
+            previous_url = "https://api.nytimes.com/svc/books/v3/lists/#{date}/hardcover-fiction.json?api-key=#{API_KEY}"
+            response = Faraday.get(previous_url)
+            @response_result = JSON.parse(response.body, { object_class: OpenStruct })
 
-    def previous_list
-        dynamic_url = ENV["NYT_API_KEY_DYNAMIC"]
-        date = "2010-05-27"
-        response = Faraday.get(dynamic_url)
-        @response_result = JSON.parse(response.body, { object_class: OpenStruct })
+            @final_result = @response_result.results.books.map do |book|
 
-        @final_result = @response_result.results.books.map do |book|
-
-            container = {
-                rank: book[:rank],
-                rank_last_week: book[:rank_last_week],
-                weeks_on_list: book[:weeks_on_list],
-                publisher: book[:publisher],
-                description: book[:description],
-                title: book[:title],
-                author: book[:author],
-                book_image: book[:book_image],
-                # TODO: add buy_links
-            }
-
-        end
-        render json: @final_result
+                container = {
+                    rank: book[:rank],
+                    rank_last_week: book[:rank_last_week],
+                    weeks_on_list: book[:weeks_on_list],
+                    publisher: book[:publisher],
+                    description: book[:description],
+                    title: book[:title],
+                    author: book[:author],
+                    book_image: book[:book_image],
+                    # TODO: add buy_links
+                }
+            end
+            render json: @final_result
     end
 
 end
