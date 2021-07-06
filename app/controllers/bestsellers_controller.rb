1c4eade0d307426fd9ea14a62c03d5e18214914f
Mickey Vershbow <mickey.vershbow@gmail.com>
2021-07-06 16:09:18 -0400

    figured out how to interpolate API key

--- app/controllers/bestsellers_controller.rb
+++ app/controllers/bestsellers_controller.rb
@@ -3,33 +3,60 @@ class BestsellersController < ApplicationController
 
     require 'faraday'
 
-    def current_list
-        current_url = ENV["NYT_API_KEY_CURRENT"]
-        response = Faraday.get(current_url)
-        @response_result = JSON.parse(response.body, { object_class: OpenStruct })
+    # def current_list
+    #     current_url = ENV["NYT_API_KEY_CURRENT"]
+    #     response = Faraday.get(current_url)
+    #     @response_result = JSON.parse(response.body, { object_class: OpenStruct })
 
-        @final_result = @response_result.results.books.map do |book|
+    #     @final_result = @response_result.results.books.map do |book|
 
-            container = {
-                rank: book[:rank],
-                rank_last_week: book[:rank_last_week],
-                weeks_on_list: book[:weeks_on_list],
-                publisher: book[:publisher],
-                description: book[:description],
-                title: book[:title],
-                author: book[:author],
-                book_image: book[:book_image],
-                # TODO: add buy_links
-            }
+    #         container = {
+    #             rank: book[:rank],
+    #             rank_last_week: book[:rank_last_week],
+    #             weeks_on_list: book[:weeks_on_list],
+    #             publisher: book[:publisher],
+    #             description: book[:description],
+    #             title: book[:title],
+    #             author: book[:author],
+    #             book_image: book[:book_image],
+    #             # TODO: add buy_links
+    #         }
 
-        end
-        render json: @final_result
+
+        def current_list
+            api_key = ENV["NYT_API_KEY_CURRENT"]
+            p api_key
+            current_url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{api_key}"
+            p current_url
+            response = Faraday.get(current_url)
+            @response_result = JSON.parse(response.body, { object_class: OpenStruct })
+
+            @final_result = @response_result.results.books.map do |book|
+
+                container = {
+                    rank: book[:rank],
+                    rank_last_week: book[:rank_last_week],
+                    weeks_on_list: book[:weeks_on_list],
+                    publisher: book[:publisher],
+                    description: book[:description],
+                    title: book[:title],
+                    author: book[:author],
+                    book_image: book[:book_image],
+                    # TODO: add buy_links
+                }
+
+            end
+            render json: @final_result
     end
 
+    #     end
+    #     render json: @final_result
+    # end
+
     def previous_list
-        # dynamic_url = ENV["NYT_API_KEY_DYNAMIC"]
+        dynamic_url = ENV["NYT_API_KEY_DYNAMIC"]
         date = "2010-05-27"
-        response = Faraday.get("https://api.nytimes.com/svc/books/v3/lists/#{date}/hardcover-fiction.json?api-key=7FxA5tYHL2GPOCHfddNn3uVDouwtx71d")
+        response = Faraday.get(dynamic_url)
         @response_result = JSON.parse(response.body, { object_class: OpenStruct })
 
         @final_result = @response_result.results.books.map do |book|
